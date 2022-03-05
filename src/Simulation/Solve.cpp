748a1a9063469ac84ee3ba35d5184991c3ecfbaa
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2022-03-04 16:00:03 -0800

    added the option of specifying a sampling frequency

--- src/Simulation/Solve.cpp
+++ src/Simulation/Solve.cpp
@@ -100,8 +100,10 @@ int Solve(  void  *s,     /*!< Array of simulation objects of type #SimulationOb
     }
 
 #ifdef with_librom
-    if (!rank) printf("libROM: taking sample.\n");
-    rom_interface.takeSample( sim, TS.waqt );
+    if (TS.iter%rom_interface.samplingFrequency() == 0) {
+      if (!rank) printf("libROM: taking sample.\n");
+      rom_interface.takeSample( sim, TS.waqt );
+    }
 #endif
 
     /* Call pre-step function */
