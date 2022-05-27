f1a6a5d837b6202c47df0abf2ff304b96d283ebf
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2022-05-27 13:17:44 -0700

    added rising thermal bubble example

---
+++ Examples/3D/NavierStokes3D/RisingThermalBubble_libROM_DMD_Train/aux/PostProcess.cpp
@@ -0,0 +1,385 @@
+/*
+  Rising Thermal Bubble:-
+  The code takes a binary solution file (that contains the 
+  conserved variable (rho, rho*u, rho*v, rho*w, e) as its input 
+  and calculates the primitive atmospheric flow variables:
+  rho, u, v, w, P, theta, pi, rho0, P0, theta0, pi0
+  and writes them to a tecplot or text file.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <math.h>
+
+typedef struct _parameters_{
+  double grav_x, grav_y, grav_z, R, gamma, P_ref, rho_ref;
+  int HB;
+} Parameters;
+
+#define _ArraySetValue_(x,size,value) \
+  {                                                                                                                 \
+    int arraycounter;                                                                                               \
+    for (arraycounter = 0; arraycounter < (size); arraycounter++)  x[arraycounter] = (value);                       \
+  }
+
+#define _ArrayIncrementIndex_(N,imax,i,done) \
+  { \
+    int arraycounter = 0; \
+    while (arraycounter < (N)) { \
+      if (i[arraycounter] == imax[arraycounter]-1) { \
+        i[arraycounter] = 0; \
+        arraycounter++; \
+      } else { \
+        i[arraycounter]++; \
+        break; \
+      } \
+    } \
+    if (arraycounter == (N)) done = 1; \
+    else          done = 0; \
+  }
+
+#define _ArrayIndex1D_(N,imax,i,ghost,index)  \
+  { \
+    index = i[N-1]+(ghost); \
+    int arraycounter; \
+    for (arraycounter = (N)-2; arraycounter > -1; arraycounter--) { \
+      index = ((index*(imax[arraycounter]+2*(ghost))) + (i[arraycounter]+(ghost))); \
+    } \
+  }
+
+double raiseto(double x, double a)
+{
+  return(exp(a*log(x)));
+}
+
+int WriteTecplot3D( int         ndims,
+                    int         nvars,
+                    int*        dim,
+                    double*     x,
+                    double*     u,
+                    const char* f,
+                    int*        index )
+{
+  if (ndims !=3) {
+    fprintf(stderr,"Error in WriteTecplot3D(): This functions is hardcoded for 3-dimensional ");
+    fprintf(stderr,"problems only. Instead, ndims=%d.\n",ndims);
+    return(1);
+  }
+  int i;
+  int imax = dim[0];
+  int jmax = dim[1];
+  int kmax = dim[2];
+
+  printf("Writing tecplot solution file %s.\n",f);
+  FILE *out;
+  out = fopen(f,"w");
+  if (!out) {
+    fprintf(stderr,"Error: could not open %s for writing.\n",f);
+    return(1);
+  }
+
+  /* writing tecplot data file headers */
+  fprintf(out,"VARIABLES=\"I\",\"J\",\"K\",\"X\",\"Y\",\"Z\",");
+  char varname[3] = "00";
+  for (i = 0; i < nvars; i++) {
+    fprintf(out,"\"%s\",",varname);
+    if (varname[1] == '9') { varname[0]++; varname[1] = '0'; }
+    else                     varname[1]++;
+  }
+  fprintf(out,"\n");
+  fprintf(out,"ZONE I=%d,J=%d,K=%d,F=POINT\n",imax,jmax,kmax);
+
+  /* writing the data */
+  int done = 0; _ArraySetValue_(index,ndims,0);
+  while (!done) {
+    int i, p;
+    _ArrayIndex1D_(ndims,dim,index,0,p);
+    for (i=0; i<ndims; i++) fprintf(out,"%4d ",index[i]);
+    for (i=0; i<ndims; i++) {
+      int j,offset = 0; for (j=0; j<i; j++) offset += dim[j];
+      fprintf(out,"%+E ",x[offset+index[i]]);
+    }
+    for (i=0; i<nvars; i++) fprintf(out,"%+E ",u[nvars*p+i]);
+    fprintf(out,"\n");
+    _ArrayIncrementIndex_(ndims,dim,index,done);
+  }
+  fclose(out);
+  return(0);
+}
+
+int WriteText(  int         ndims,
+                int         nvars,
+                int*        dim,
+                double*     x,
+                double*     u,
+                const char* f,
+                int*        index )
+{
+  FILE *out;
+  out = fopen(f,"w");
+  if (!out) {
+    fprintf(stderr,"Error: could not open %s for writing.\n",f);
+    return(1);
+  }
+
+  int done = 0; _ArraySetValue_(index,ndims,0);
+  while (!done) {
+    int i, p;
+    _ArrayIndex1D_(ndims,dim,index,0,p);
+    for (i=0; i<ndims; i++) fprintf(out,"%4d ",index[i]);
+    for (i=0; i<ndims; i++) {
+      int j,offset = 0; for (j=0; j<i; j++) offset += dim[j];
+      fprintf(out,"%+E ",x[offset+index[i]]);
+    }
+    for (i=0; i<nvars; i++) fprintf(out,"%+E ",u[nvars*p+i]);
+    fprintf(out,"\n");
+    _ArrayIncrementIndex_(ndims,dim,index,done);
+  }
+  fclose(out);
+  return(0);
+}
+
+int PostProcess(const std::string&  fname, 
+                const std::string&  oname, 
+                void*               p, 
+                int                 flag  )
+{
+  Parameters *params = (Parameters*) p;
+  FILE *in; in = fopen(fname.c_str(),"rb");
+
+  if (!in) return(-1);
+
+  printf("Reading file %s.\n",fname.c_str());
+  int ndims, nvars;
+  double *U,*x;
+
+  /* read the file headers */
+  fread(&ndims,sizeof(int),1,in);
+  fread(&nvars,sizeof(int),1,in);
+
+  /* some checks */
+  if (ndims != 3) {
+    printf("Error: ndims in %s not equal to 3!\n",fname.c_str());
+    return(1);
+  }
+  if (nvars != 5) {
+    printf("Error: nvars in %s not equal to 5!\n",fname.c_str());
+    return(1);
+  }
+
+  /* read dimensions */
+  int dims[ndims];
+  fread(dims,sizeof(int),ndims,in);
+  printf("Dimensions: %d x %d x %d\n",dims[0],dims[1],dims[2]);
+  printf("Nvars     : %d\n",nvars);
+
+  /* allocate grid and solution arrays */
+  x = (double*) calloc (dims[0]+dims[1]+dims[2]       ,sizeof(double));
+  U = (double*) calloc (dims[0]*dims[1]*dims[2]*nvars ,sizeof(double));
+
+  /* read grid and solution */
+  fread(x,sizeof(double),dims[0]+dims[1]+dims[2]      ,in);
+  fread(U,sizeof(double),dims[0]*dims[1]*dims[2]*nvars,in);
+  /* done reading */
+  fclose(in);
+
+  int imax = dims[0];
+  int jmax = dims[1];
+  int kmax = dims[2];
+
+  /* allocate primitive variable array (rho, u, v, w, P, theta, rho0, P0, pi0, theta0) */
+  int evars = 5;
+  double *Q = (double*) calloc ((nvars+evars)*imax*jmax*kmax,sizeof(double));
+    
+  /* calculate primitive variables */
+  int i, j, k;
+  double *X           = x;
+  double *Y           = x+imax;
+  double *Z           = x+imax+jmax;
+  double grav_y       = params->grav_y;
+  double R            = params->R;
+  double gamma        = params->gamma;
+  double P_ref        = params->P_ref;
+  double rho_ref      = params->rho_ref;
+  double T_ref        = P_ref / (R*rho_ref);
+  double inv_gamma_m1 = 1.0 / (gamma-1.0);
+  double Cp           = gamma * inv_gamma_m1 * R;
+
+  for (i=0; i<imax; i++) {
+    for (j=0; j<jmax; j++) {
+      for (k=0; k<kmax; k++) {
+        int p = i + imax*j + imax*jmax*k;
+
+        double rho0, theta0, Pexner, P0;
+        theta0  = T_ref;
+        Pexner  = 1.0 - (grav_y*Y[j])/(Cp*T_ref);
+        rho0    = (P_ref/(R*theta0)) * raiseto(Pexner, inv_gamma_m1);
+        P0      = P_ref   * raiseto(Pexner, gamma*inv_gamma_m1);
+
+        double rho, uvel, vvel, wvel, E, P, theta;
+        rho   = U[nvars*p+0];
+        uvel  = U[nvars*p+1] / rho;
+        vvel  = U[nvars*p+2] / rho;
+        wvel  = U[nvars*p+3] / rho;
+        E     = U[nvars*p+4];
+        P     = (gamma-1.0) * (E - 0.5*rho*(uvel*uvel+vvel*vvel+wvel*wvel));
+        theta = (E-0.5*rho*(uvel*uvel+vvel*vvel+wvel*wvel))/(Pexner*rho) * ((gamma-1.0)/R);
+
+        Q[(nvars+evars)*p+0] = rho;
+        Q[(nvars+evars)*p+1] = uvel;
+        Q[(nvars+evars)*p+2] = vvel;
+        Q[(nvars+evars)*p+3] = wvel;
+        Q[(nvars+evars)*p+4] = P;
+        Q[(nvars+evars)*p+5] = theta;
+        Q[(nvars+evars)*p+6] = rho0;
+        Q[(nvars+evars)*p+7] = P0;
+        Q[(nvars+evars)*p+8] = Pexner;
+        Q[(nvars+evars)*p+9] = theta0;
+      }
+    }
+  }
+
+  int dim[ndims], index[ndims];
+  dim[0] = imax;
+  dim[1] = jmax;
+  dim[2] = kmax;
+
+  /* write Tecplot/Text file */
+  if (flag) WriteTecplot3D(ndims,nvars+evars,&dim[0],x,Q,oname.c_str(),&index[0]);
+  else      WriteText     (ndims,nvars+evars,&dim[0],x,Q,oname.c_str(),&index[0]);
+
+  /* clean up */
+  free(U);
+  free(Q);
+  free(x);
+}
+
+int main()
+{
+  FILE *inputs;
+  char op_file_format[50], overwrite[50];
+
+  int flag;
+  printf("Write tecplot file (1) or plain text file (0): ");
+  scanf("%d",&flag);
+
+  if ((flag != 1) && (flag != 0)) {
+    printf("Error: Invalid input. Should be 1 or 0.\n");
+    return(0);
+  }
+
+  printf("Reading solver.inp.\n");
+  inputs = fopen("solver.inp","r");
+  if (!inputs) {
+    fprintf(stderr,"Error: File \"solver.inp\" not found.\n");
+    return(1);
+  } else {
+	  char word[100];
+    fscanf(inputs,"%s",word);
+    if (!strcmp(word, "begin")){
+	    while (strcmp(word, "end")){
+		    fscanf(inputs,"%s",word);
+   			if      (!strcmp(word, "op_file_format"   ))  fscanf(inputs,"%s" ,op_file_format);
+   			else if (!strcmp(word, "op_overwrite"     ))  fscanf(inputs,"%s" ,overwrite      );
+      }
+    }
+    fclose(inputs);
+  }
+  if (strcmp(op_file_format,"binary") && strcmp(op_file_format,"bin")) {
+    printf("Error: solution output needs to be in binary files.\n");
+    return(0);
+  }
+
+  Parameters params;
+  /* default values */
+  params.grav_x   = 0.0;
+  params.grav_y   = 9.8;
+  params.grav_z   = 0.0;
+  params.R        = 287.058;
+  params.gamma    = 1.4;
+  params.P_ref    = 100000.0;
+  params.rho_ref  = 100000.0 / (params.R * 300.0);
+  params.HB       = 0;
+  /* read these parameters from file */
+  printf("Reading physics.inp.\n");
+  inputs = fopen("physics.inp","r");
+  if (!inputs) {
+    fprintf(stderr,"Error: File \"physics.inp\" not found.\n");
+    return(1);
+  } else {
+	  char word[100];
+    fscanf(inputs,"%s",word);
+    if (!strcmp(word, "begin")){
+	    while (strcmp(word, "end")){
+		    fscanf(inputs,"%s",word);
+   			if      (!strcmp(word, "gamma"))    fscanf(inputs,"%lf",&params.gamma);
+   			else if (!strcmp(word, "gravity")) {
+          fscanf(inputs,"%lf",&params.grav_x);
+          fscanf(inputs,"%lf",&params.grav_y);
+          fscanf(inputs,"%lf",&params.grav_z);
+        } else if (!strcmp(word,"p_ref"))   fscanf(inputs,"%lf",&params.P_ref);
+        else if   (!strcmp(word,"rho_ref")) fscanf(inputs,"%lf",&params.rho_ref);
+        else if   (!strcmp(word,"HB"))      fscanf(inputs,"%d",&params.HB);
+      }
+    }
+    fclose(inputs);
+  }
+
+  std::string op_fname_extn = ".bin";
+  std::string pp_fname_extn = ".dat";
+  if (!strcmp(overwrite,"no")) {
+    {
+      std::string op_fname_root = "op_";
+      int counter = 0;
+      while(1) {
+        /* set filename */
+        char counter_str[6];
+        sprintf(counter_str, "%05d", counter);
+        std::string filename = op_fname_root + std::string(counter_str) + op_fname_extn;
+        std::string tecfile = op_fname_root + std::string(counter_str) + pp_fname_extn;
+        int err = PostProcess(filename, tecfile, &params, flag);
+        if (err == -1) {
+          printf("No more files with prefix %s found.\n", op_fname_root.c_str());
+          break;
+        }
+        counter++;
+      }
+    }
+    {
+      std::string op_fname_root = "op_rom_";
+      int counter = 0;
+      while(1) {
+        /* set filename */
+        char counter_str[6];
+        sprintf(counter_str, "%05d", counter);
+        std::string filename = op_fname_root + std::string(counter_str) + op_fname_extn;
+        std::string tecfile = op_fname_root + std::string(counter_str) + pp_fname_extn;
+        int err = PostProcess(filename, tecfile, &params, flag);
+        if (err == -1) {
+          printf("No more files with prefix %s found.\n", op_fname_root.c_str());
+          break;
+        }
+        counter++;
+      }
+    }
+  } else if (!strcmp(overwrite,"yes")) {
+    {
+      std::string op_fname_root = "op";
+      /* set filename */
+      std::string filename = op_fname_root + op_fname_extn;
+      std::string tecfile = op_fname_root + pp_fname_extn;
+      int err = PostProcess(filename, tecfile, &params, flag);
+    }
+    {
+      std::string op_fname_root = "op_rom";
+      /* set filename */
+      std::string filename = op_fname_root + op_fname_extn;
+      std::string tecfile = op_fname_root + pp_fname_extn;
+      int err = PostProcess(filename, tecfile, &params, flag);
+    }
+  }
+
+  return(0);
+}
---
+++ Examples/3D/NavierStokes3D/RisingThermalBubble_libROM_DMD_Train/plotSolution.py
@@ -0,0 +1,413 @@
+'''
+Python script to create plots of atmoshpheric flow variables
+of a HyPar simulation of the rising thermal bubble case. 
+This particular file is for the
+3D Navier-Stokes/Euler physics, where the solution vector
+components are (rho, rho*u, rho*v, e).
+
+- if op_overwrite is set to "no", a plot is generated
+for each variable (solution vector component) and each
+simulation time for which the solution is available.
+- if op_overwrite is set to "yes", a single plot is
+created for for each variable (solution vector component).
+- solution must be in binary format
+
+Make sure the environment variable "HYPAR_DIR" is set
+and points to the correct location (/path/to/hypar)
+'''
+
+import os
+import time
+import sys
+
+import numpy as np
+import numpy.linalg as LA
+import matplotlib
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from skimage import measure
+
+hypar_dir = os.environ.get('HYPAR_DIR')
+hypar_dir_python = hypar_dir + '/Examples/Python'
+sys.path.append(hypar_dir_python)
+
+import modHyParUtils as hyparutils
+
+font = {'size':22}
+matplotlib.rc('font', **font)
+colormap='jet'
+
+plt_dir_name='plots'
+fig_nv=3
+fig_nh=1
+figsize=(1.4*fig_nh*10,fig_nv*10)
+
+density = 4
+linewidth = 1
+slice_loc = 0.5
+
+'''
+Set up the simulation parameters
+'''
+sim_path = '.'
+sim_inp_data = hyparutils.readHyParInpFile(sim_path+'/simulation.inp')
+solver_inp_data = hyparutils.readHyParInpFile(sim_path+'/solver.inp')
+physics_inp_data = hyparutils.readHyParInpFile(sim_path+'/physics.inp')
+
+if not sim_inp_data:
+    nsims = 1
+else:
+    nsims = int(sim_inp_data['nsims'][0])
+
+print('Number of simulations: ', nsims)
+
+if solver_inp_data['op_file_format'] != 'binary':
+    raise Exception("op_file_format must be 'binary' in solver.inp.")
+
+ndims = int(solver_inp_data['ndims'][0])
+nvars = int(solver_inp_data['nvars'][0])
+size = np.int32(solver_inp_data['size'])
+
+R = 287.058
+T_ref = 300.0
+try:
+  grav = np.float32(physics_inp_data['gravity'])
+except:
+  grav = np.float32(np.array([0.0, 9.8, 0.0]))
+try:
+  gamma = np.float32(physics_inp_data['gamma'])[0]
+except:
+  gamma = 1.4
+try:
+  p_ref = np.float32(physics_inp_data['p_ref'])[0]
+except:
+  p_ref = 100000.0
+rho_ref = p_ref / (R*T_ref)
+Cp = gamma / (gamma-1.0) * R
+
+print('Atmospheric Flow Paramters (SI Units):')
+print('  R        = ', R)
+print('  T_ref    = ', T_ref)
+print('  p_ref    = ', p_ref)
+print('  rho_ref  = ', rho_ref)
+print('  gamma    = ', gamma)
+print('  gravity  = ', grav)
+print('  Cp       = ', Cp)
+
+def convertVar( u_conserved: np.ndarray,
+                y: np.ndarray ) -> np.ndarray:
+  theta0 = T_ref
+  p_exner = 1.0 - (grav[1]*y)/(Cp*T_ref)
+  rho0 = (p_ref/(R*theta0)) * np.power(p_exner, (1.0/(gamma-1.0)))
+  p0 = p_ref * np.power(p_exner, (gamma/(gamma-1.0)))
+
+  rho = u_conserved[0,:,:,:]
+  uvel = u_conserved[1,:,:,:] / rho
+  vvel = u_conserved[2,:,:,:] / rho
+  wvel = u_conserved[3,:,:,:] / rho
+  ene = u_conserved[4,:,:,:]
+  ke = 0.5 * (np.square(uvel) + np.square(vvel) + np.square(wvel))
+  pressure = (gamma-1.0) * (ene - rho*ke)
+  theta = ((gamma-1.0)/R) * (ene - rho*ke) / (rho*p_exner)
+
+  retval = np.stack([ rho,
+                      uvel,
+                      vvel,
+                      wvel,
+                      pressure,
+                      theta,
+                      rho0,
+                      p0,
+                      p_exner,
+                      theta0*np.ones(rho.shape)], 
+                    axis=0)
+  return retval
+
+def plotData( data_fom_3d: np.ndarray,
+              data_rom_3d: np.ndarray,
+              x3d: np.ndarray,
+              y3d: np.ndarray,
+              z3d: np.ndarray,
+              fig_filename,
+              varname, 
+              t_solution ):
+
+  # For ease of visualization, switch y and z. In the simulation,
+  # y is the vertical axis, but that makes things difficult for a 
+  # 3D plot!
+  data_fom_3d = np.transpose(data_fom_3d,[0,2,1])
+  data_rom_3d = np.transpose(data_rom_3d,[0,2,1])
+  data_del_3d = data_fom_3d - data_rom_3d
+
+  fom_sol_norm = LA.norm(data_fom_3d.ravel())
+  diff_norm = LA.norm(data_del_3d.ravel())
+  if fom_sol_norm > 1e-14:
+    diff_norm = diff_norm / fom_sol_norm
+
+  fig = plt.figure(figsize=figsize)
+
+  kw1 =  {'vmin': data_fom_3d.min(),
+          'vmax': data_fom_3d.max(),
+          'levels': np.linspace((data_fom_3d.min()+0.1*(data_fom_3d.max()-data_fom_3d.min())),
+                                (data_fom_3d.max()-0.1*(data_fom_3d.max()-data_fom_3d.min())),
+                                25),
+          'antialiased' : True,
+          'alpha' : 0.8,
+          'cmap' : colormap,
+        }
+  kw2 =  {'vmin': data_fom_3d.min(),
+          'vmax': data_fom_3d.max(),
+          'levels': np.linspace(data_fom_3d.min(),
+                                data_fom_3d.max(),
+                                100),
+          'antialiased' : True,
+          'alpha' : 0.6,
+          'cmap' : colormap,
+        }
+  ax = fig.add_subplot(fig_nv, fig_nh, 1, projection='3d')
+  plot_contour1= ax.contour(  x3d[:, 0, :], 
+                              data_fom_3d[:,int(slice_loc*data_fom_3d.shape[1]),:], 
+                              z3d[:, 0, :], 
+                              zdir='y', offset=y.max()/2, **kw1 )
+  plot_contour2= ax.contourf( data_fom_3d[int(slice_loc*data_fom_3d.shape[0]), :, :], 
+                              y3d[0, :, :], 
+                              z3d[0, :, :], 
+                              zdir='x', offset=x.max()/2, **kw2 )
+  cb = fig.colorbar(plot_contour2, ax=ax)
+  cb.ax.set_yticklabels(["{:.2f}".format(i) for i in cb.get_ticks()])
+
+  ax.set_xlabel("x (m)")
+  ax.set_ylabel("z (m)")
+  ax.set_zlabel("y (m)")
+  ax.set_title("FOM {:}, t={:.1f}".format(varname, t_solution))
+  ax.set_xlim(np.min(x3d), np.max(x3d))
+  ax.set_ylim(np.min(y3d), np.max(y3d))
+  ax.set_zlim(np.min(z3d), np.max(z3d))
+  ax.view_init(10, 210)
+  ax.dist = 9
+  
+  kw1 =  {'vmin': data_rom_3d.min(),
+          'vmax': data_rom_3d.max(),
+          'levels': np.linspace((data_rom_3d.min()+0.1*(data_rom_3d.max()-data_rom_3d.min())),
+                                (data_rom_3d.max()-0.1*(data_rom_3d.max()-data_rom_3d.min())),
+                                25),
+          'antialiased' : True,
+          'alpha' : 0.8,
+          'cmap' : colormap,
+        }
+  kw2 =  {'vmin': data_rom_3d.min(),
+          'vmax': data_rom_3d.max(),
+          'levels': np.linspace(data_rom_3d.min(),
+                                data_rom_3d.max(),
+                                100),
+          'antialiased' : True,
+          'alpha' : 0.6,
+          'cmap' : colormap,
+        }
+  ax = fig.add_subplot(fig_nv, fig_nh, 2, projection='3d')
+  plot_contour1= ax.contour(  x3d[:, 0, :], 
+                              data_rom_3d[:,int(slice_loc*data_rom_3d.shape[1]),:], 
+                              z3d[:, 0, :], 
+                              zdir='y', offset=y.max()/2, **kw1 )
+  plot_contour2= ax.contourf( data_rom_3d[int(slice_loc*data_rom_3d.shape[0]), :, :], 
+                              y3d[0, :, :], 
+                              z3d[0, :, :], 
+                              zdir='x', offset=x.max()/2, **kw2 )
+  cb = fig.colorbar(plot_contour2, ax=ax)
+  cb.ax.set_yticklabels(["{:.2f}".format(i) for i in cb.get_ticks()])
+  ax.set_xlabel("x (m)")
+  ax.set_ylabel("z (m)")
+  ax.set_zlabel("y (m)")
+  ax.set_title("ROM {:}, t={:.1f}".format(varname, t_solution))
+  ax.set_xlim(np.min(x3d), np.max(x3d))
+  ax.set_ylim(np.min(y3d), np.max(y3d))
+  ax.set_zlim(np.min(z3d), np.max(z3d))
+  ax.view_init(10, 210)
+  ax.dist = 9
+  
+  kw1 =  {'vmin': data_del_3d.min(),
+          'vmax': data_del_3d.max(),
+          'levels': np.linspace((data_del_3d.min()+0.1*(data_del_3d.max()-data_del_3d.min())),
+                                (data_del_3d.max()-0.1*(data_del_3d.max()-data_del_3d.min())),
+                                25),
+          'antialiased' : True,
+          'alpha' : 0.8,
+          'cmap' : colormap,
+        }
+  kw2 =  {'vmin': data_del_3d.min(),
+          'vmax': data_del_3d.max(),
+          'levels': np.linspace(data_del_3d.min(),
+                                data_del_3d.max(),
+                                100),
+          'antialiased' : True,
+          'alpha' : 0.6,
+          'cmap' : colormap,
+        }
+  ax = fig.add_subplot(fig_nv, fig_nh, 3, projection='3d')
+  plot_contour1= ax.contour(  x3d[:, 0, :], 
+                              data_del_3d[:,int(slice_loc*data_del_3d.shape[1]),:], 
+                              z3d[:, 0, :], 
+                              zdir='y', offset=y.max()/2, **kw1 )
+  plot_contour2= ax.contourf( data_del_3d[int(slice_loc*data_del_3d.shape[0]), :, :], 
+                              y3d[0, :, :], 
+                              z3d[0, :, :], 
+                              zdir='x', offset=x.max()/2, **kw2 )
+  cb = fig.colorbar(plot_contour2, ax=ax)
+  cb.ax.set_yticklabels(["{:.1e}".format(i) for i in cb.get_ticks()])
+  ax.set_xlabel("x (m)")
+  ax.set_ylabel("z (m)")
+  ax.set_zlabel("y (m)")
+  ax.set_title("Diff {:}, t={:.1f}, norm={:.3e}".format(varname, 
+                                                      t_solution, 
+                                                      diff_norm))
+  ax.set_xlim(np.min(x3d), np.max(x3d))
+  ax.set_ylim(np.min(y3d), np.max(y3d))
+  ax.set_zlim(np.min(z3d), np.max(z3d))
+  ax.view_init(10, 210)
+  ax.dist = 9
+  
+  plt.tight_layout()
+  print('Saving %s' % fig_filename)
+  plt.savefig(fig_filename)
+  plt.close()
+
+if not os.path.exists(plt_dir_name):
+      os.makedirs(plt_dir_name)
+
+if solver_inp_data['op_overwrite'] == 'no':
+
+  niter = int(solver_inp_data['n_iter'][0])
+  dt = float(solver_inp_data['dt'][0])
+  t_final = dt*niter
+  
+  op_write_iter = int(solver_inp_data['file_op_iter'][0])
+  dt_snapshots = op_write_iter*dt
+  n_snapshots = int(niter/op_write_iter) + 1
+  
+  print('Simulation parameters:')
+  print('  ndims = ', ndims)
+  print('  nvars = ', nvars)
+  print('  grid size = ', size)
+  print('  simulation dt = ', dt)
+  print('  niter = ', niter)
+  print('  final time = ', t_final)
+  print('  snapshot dt = ', dt_snapshots)
+  print('  number of snapshots = ', n_snapshots)
+
+  for i in range(n_snapshots):
+    for sim in range(nsims):
+    
+      '''
+      Load simulation data (solution snapshots)
+      '''
+      op_root='op'
+      if nsims >= 100:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:03d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims >= 10:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:02d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims > 1:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:01d}'+'_'+f'{i:05d}'+'.bin'
+      else:
+        fname = sim_path + '/'+op_root+'_'+f'{i:05d}'+'.bin'
+      grid,solution_fom = hyparutils.readOpFile(fname, ndims, nvars, size)
+      solution_fom = np.float32(solution_fom)
+      op_root='op_rom'
+      if nsims >= 100:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:03d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims >= 10:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:02d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims > 1:
+        fname = sim_path + '/'+op_root+'_'+f'{sim:01d}'+'_'+f'{i:05d}'+'.bin'
+      else:
+        fname = sim_path + '/'+op_root+'_'+f'{i:05d}'+'.bin'
+      grid,solution_rom = hyparutils.readOpFile(fname, ndims, nvars, size)
+      solution_rom = np.float32(solution_rom)
+
+      x = grid[:size[0]]
+      y = grid[size[0]:(size[0]+size[1])]
+      z = grid[(size[0]+size[1]):]
+      y3d, x3d, z3d = np.meshgrid(x,y,z)
+  
+      u_fom_cons_3d = np.transpose(solution_fom.reshape(size[2],size[1],size[0],nvars))
+      u_fom_prim_3d = convertVar(u_fom_cons_3d, y3d)
+  
+      u_rom_cons_3d = np.transpose(solution_rom.reshape(size[2],size[1],size[0],nvars))
+      u_rom_prim_3d = convertVar(u_rom_cons_3d, y3d)
+  
+      theta_fom = u_fom_prim_3d[5,:,:,:]
+      theta_rom = u_rom_prim_3d[5,:,:,:]
+      if nsims > 1:
+        plt_fname = plt_dir_name+'/fig_theta_'+f'{s:02d}'+'_'+f'{i:05d}'+'.png'
+      else:
+        plt_fname = plt_dir_name+'/fig_theta_'+f'{i:05d}'+'.png'
+      plotData( theta_fom, 
+                theta_rom, 
+                x3d, y3d, z3d, 
+                plt_fname, 
+                'theta', 
+                i*dt_snapshots)
+
+else:
+
+  niter = int(solver_inp_data['n_iter'][0])
+  dt = float(solver_inp_data['dt'][0])
+  t_final = dt*niter
+  
+  print('Simulation parameters:')
+  print('  ndims = ', ndims)
+  print('  nvars = ', nvars)
+  print('  grid size = ', size)
+  print('  final time = ', t_final)
+
+  for sim in range(nsims):
+  
+    '''
+    Load simulation data (solution snapshots)
+    '''
+    op_root='op'
+    if nsims >= 100:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:03d}'+'.bin'
+    elif nsims >= 10:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:02d}'+'.bin'
+    elif nsims > 1:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:01d}'+'.bin'
+    else:
+      fname = sim_path + '/'+op_root+'.bin'
+    grid,solution_fom = hyparutils.readOpFile(fname, ndims, nvars, size)
+    solution_fom = np.float32(solution_fom)
+    op_root='op_rom'
+    if nsims >= 100:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:03d}'+'.bin'
+    elif nsims >= 10:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:02d}'+'.bin'
+    elif nsims > 1:
+      fname = sim_path + '/'+op_root+'_'+f'{sim:01d}'+'.bin'
+    else:
+      fname = sim_path + '/'+op_root+'.bin'
+    grid,solution_rom = hyparutils.readOpFile(fname, ndims, nvars, size)
+    solution_rom = np.float32(solution_rom)
+
+    x = grid[:size[0]]
+    y = grid[size[0]:(size[0]+size[1])]
+    z = grid[(size[0]+size[1]):]
+    y3d, x3d, z3d = np.meshgrid(x,y,z)
+
+    u_fom_cons_3d = np.transpose(solution_fom.reshape(size[2],size[1],size[0],nvars))
+    u_fom_prim_3d = convertVar(u_fom_cons_3d, y3d)
+
+    u_rom_cons_3d = np.transpose(solution_rom.reshape(size[2],size[1],size[0],nvars))
+    u_rom_prim_3d = convertVar(u_rom_cons_3d, y3d)
+
+    theta_fom = u_fom_prim_3d[5,:,:,:]
+    theta_rom = u_rom_prim_3d[5,:,:,:]
+    if nsims > 1:
+      plt_fname = plt_dir_name+'/fig_theta_'+f'{s:02d}'+'.png'
+    else:
+        plt_fname = plt_dir_name+'/fig_theta.png'
+    plotData( theta_fom, 
+              theta_rom, 
+              x3d, y3d, z3d, 
+              plt_fname, 'theta', 
+              t_final )
+
