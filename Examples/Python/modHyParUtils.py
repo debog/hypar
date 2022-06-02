a9b2a2faa7e6023ffa087f1e3f58314f71e9fd7e
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2022-06-01 17:07:31 -0700

    updated python postprocessing module

--- Examples/Python/modHyParUtils.py
+++ Examples/Python/modHyParUtils.py
@@ -9,7 +9,7 @@ Input arguments:
 The function will return a dictionary with the keywords found
 in the file.
 '''
-def readHyParInpFile(fname):
+def readHyParInpFile(fname: str):
   input_data = {}
 
   try:
@@ -28,7 +28,8 @@ def readHyParInpFile(fname):
         
   return input_data
 
-def readHyParErrFile(path, ndims):
+def readHyParErrFile( path: str, 
+                      ndims: int):
 
   fname = path + '/errors.dat'
   with open(fname,'r') as file:
@@ -51,7 +52,10 @@ Input arguments:
 
 Returns a tuple with the grid x and the solution u.
 '''
-def readOpFile(fname, a_ndims, a_nvars, a_size):
+def readOpFile( fname: str, 
+                a_ndims: int, 
+                a_nvars: int, 
+                a_size: np.array):
   with open(fname, "rb") as f_op:
     ndims = np.fromfile(f_op, dtype=np.int32, count=1)[0]
     if ndims != a_ndims:
@@ -69,33 +73,95 @@ def readOpFile(fname, a_ndims, a_nvars, a_size):
     u = np.fromfile(f_op, dtype=np.float64, count=np.prod(size)*nvars)
 
     return x, u
+
+'''
+For an unsteady simulation, this function reads in the solution
+from each op_sim_idx_<index>.bin file and creates a 2D matrix whose rows
+are the serialized solution vector at a particular simulation time.
+
+Input arguments:
+  path        : directory path where the op_* files are located
+  nsims       : total number of simulation domains
+  sim_idx     : the index of the simulation for which to
+                read the solution files (0 <= sim_idx < nsims)
+  n_op_files  : number of snapshot files to read
+  ndims       : number of spatial dimensions
+  nvars       : number of vector components at each grid point
+  size        : integer array with grid size in each dimension
+(Optional)
+  op_root     : filename root for output files
+'''
+def getSimulationSnapshots( path: str, 
+                            nsims: int,
+                            sim_idx: int, 
+                            n_op_files: int, 
+                            ndims: int, 
+                            nvars: int, 
+                            size: np.ndarray,
+                            op_root: str ='op'):
+  ndof = nvars * np.prod(size)
+  snapshots = np.empty((0,ndof),np.float64)
+  if n_op_files > 1:
+    for i in range(n_op_files):
+      if nsims >= 100:
+        fname = path + '/'+op_root+'_'+f'{sim_idx:03d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims >= 10:
+        fname = path + '/'+op_root+'_'+f'{sim_idx:02d}'+'_'+f'{i:05d}'+'.bin'
+      elif nsims > 1:
+        fname = path + '/'+op_root+'_'+f'{sim_idx:01d}'+'_'+f'{i:05d}'+'.bin'
+      else:
+        fname = path + '/'+op_root+'_'+f'{i:05d}'+'.bin'
+      x, u = readOpFile(fname, ndims, nvars, size)
+      snapshots = np.concatenate((snapshots,np.expand_dims(u,axis=0)),axis=0)
+  else:
+    if nsims >= 100:
+      fname = path + '/'+op_root+'_'+f'{sim_idx:03d}'+'.bin'
+    elif nsims >= 10:
+      fname = path + '/'+op_root+'_'+f'{sim_idx:02d}'+'.bin'
+    elif nsims > 1:
+      fname = path + '/'+op_root+'_'+f'{sim_idx:01d}'+'.bin'
+    else:
+      fname = path + '/'+op_root+'.bin'
+    x, u = readOpFile(fname, ndims, nvars, size)
+    snapshots = np.concatenate((snapshots,np.expand_dims(u,axis=0)),axis=0)
+
+  return x,snapshots
+
 '''
 For an unsteady simulation, this function reads in the solution
 from each op_<index>.bin file and creates a 2D matrix whose rows
 are the serialized solution vector at a particular simulation time.
 
 Input arguments:
-  path: directory path where the op_* files are located
-  n_op_files: number of snapshot files to read
-  ndims: number of spatial dimensions
-  nvars: number of vector components at each grid point
-  size: integer array with grid size in each dimension
+  path        : directory path where the op_* files are located
+  nsims       : total number of simulation domains
+  n_op_files  : number of snapshot files to read
+  ndims       : number of spatial dimensions
+  nvars       : number of vector components at each grid point
+  size        : integer array with grid size in each dimension
 (Optional)
-  op_root: filename root for output files
+  op_root     : filename root for output files
+
+NOTE: for nsims > 1, the grid size for each simulation domain
+must be exactly the same!
 '''
-def getSolutionSnapshots( path, 
-                          nsims, 
-                          n_op_files, 
-                          ndims, 
-                          nvars, 
-                          size,
-                          op_root='op'):
+def getSolutionSnapshots( path: str, 
+                          nsims: int, 
+                          n_op_files: int, 
+                          ndims: int, 
+                          nvars: int, 
+                          size: np.ndarray,
+                          op_root: str ='op' ):
   if nsims > 1:
     ndof = nvars * np.prod(size[0,:])
   else:
     ndof = nvars * np.prod(size)
   snapshots = np.empty((0,ndof),np.float64)
   for sim in range(nsims):
+    if nsims > 1:
+      size_sim = size[sim,:]
+    else:
+      size_sim = size
     if n_op_files > 1:
       for i in range(n_op_files):
         if nsims >= 100:
@@ -106,7 +172,7 @@ def getSolutionSnapshots( path,
           fname = path + '/'+op_root+'_'+f'{sim:01d}'+'_'+f'{i:05d}'+'.bin'
         else:
           fname = path + '/'+op_root+'_'+f'{i:05d}'+'.bin'
-        x, u = readOpFile(fname, ndims, nvars, size)
+        x, u = readOpFile(fname, ndims, nvars, size_sim)
         snapshots = np.concatenate((snapshots,np.expand_dims(u,axis=0)),axis=0)
     else:
       if nsims >= 100:
@@ -117,7 +183,7 @@ def getSolutionSnapshots( path,
         fname = path + '/'+op_root+'_'+f'{sim:01d}'+'.bin'
       else:
         fname = path + '/'+op_root+'.bin'
-      x, u = readOpFile(fname, ndims, nvars, size)
+      x, u = readOpFile(fname, ndims, nvars, size_sim)
       snapshots = np.concatenate((snapshots,np.expand_dims(u,axis=0)),axis=0)
 
   return x,snapshots
