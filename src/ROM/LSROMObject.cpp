e8420aa3f7fa40b66ac9755064f44199454f78da
Ping-Hsuan <pht2@illinois.edu>
2023-06-09 11:44:38 -0700

    compute first and second order derivative of potential field

--- src/ROM/LSROMObject.cpp
+++ src/ROM/LSROMObject.cpp
@@ -45,8 +45,8 @@ extern "C" int  CalculateROMDiff(void*,void*);
 extern "C" void ResetFilenameIndex(char*, int); /*!< Reset filename index */
 extern "C" void IncrementFilenameIndex(char*,int);
 extern "C" int  VlasovWriteSpatialField(void*, void*, double*);
-extern "C" int FirstDerivativeSecondOrderCentral1D (double*,double*,int,int,void*,void*);
-extern "C" int FirstDerivativeSecondOrderCentral (double*,double*,int,int,void*,void*);
+extern "C" int FirstDerivativeSecondOrderCentralNoGhosts (double*,double*,int,int,void*,void*);
+extern "C" int SecondDerivativeSecondOrderCentralNoGhosts (double*,double*,int,void*,void*);
 
 LSROMObject::LSROMObject(   const int     a_vec_size, /*!< vector size */
                             const double  a_dt,       /*!< time step size */
@@ -269,31 +269,48 @@ void LSROMObject::takeSample(  const CAROM::Vector& a_U, /*!< solution vector */
 //  }
 //  std::cout << std::endl;
 //}
+  if (!m_rank) {
+    std::cout << "checking e field (no ghosts) \n";
+    for (int i = 0; i < vec_wghosts.size(); i++) {
+          std::cout << "Index " << i << ": " << vec_wghosts[i] << std::endl;
+    }
+    std::cout << std::endl;
+  }
   ::VlasovWriteSpatialField(&(sim[0].solver),&(sim[0].mpi),param->potential);
-//::FirstDerivativeSecondOrderCentral1D(vec_x_wghosts.data(),param->potential,0,1,&(sim[0].solver),&(sim[0].mpi));
-//MPIExchangeBoundaries1D(mpi, vec_x_wghosts.data(), solver->dim_local[0], solver->ghosts, 0, solver->ndims);
-//if (!m_rank) {
-//  std::cout << "checking vec_x_wghosts\n";
-//  for (int i = 0; i < vec_x_wghosts.size(); i++) {
-//        std::cout << "Index " << i << ": " << -vec_x_wghosts[i]*solver->dxinv[0] << std::endl;
-//  }
-//  std::cout << std::endl;
-//}
-//ArrayCopynD(1,
-//            vec_x_wghosts.data(),
-//            vec_wghosts.data(),
-//            sim[0].solver.dim_local,
-//            sim[0].solver.ghosts,
-//            0,
-//            index.data(),
-//            sim[0].solver.nvars);
-//if (!m_rank) {
-//  std::cout << "checking vec_wghosts \n";
-//  for (int i = 0; i < vec_wghosts.size(); i++) {
-//        std::cout << "Index " << i << ": " << -vec_wghosts[i]*solver->dxinv[0] << std::endl;
-//  }
-//  std::cout << std::endl;
-//}
+  ::FirstDerivativeSecondOrderCentralNoGhosts(vec_x_wghosts.data(),param->potential,0,1,&(sim[0].solver),&(sim[0].mpi));
+  MPIExchangeBoundaries1D(mpi, vec_x_wghosts.data(), solver->dim_local[0], solver->ghosts, 0, solver->ndims);
+  if (!m_rank) {
+    std::cout << "checking grad phi (ghosts)\n";
+    for (int i = 0; i < vec_x_wghosts.size(); i++) {
+          std::cout << "Index " << i << ": " << -vec_x_wghosts[i]*solver->dxinv[0] << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  ArrayCopynD(1,
+              vec_x_wghosts.data(),
+              vec_wghosts.data(),
+              sim[0].solver.dim_local,
+              sim[0].solver.ghosts,
+              0,
+              index.data(),
+              sim[0].solver.nvars);
+  if (!m_rank) {
+    std::cout << "checking -grad phi (no ghosts)\n";
+    for (int i = 0; i < vec_wghosts.size(); i++) {
+          std::cout << "Index " << i << ": " << -vec_wghosts[i]*solver->dxinv[0] << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  ::SecondDerivativeSecondOrderCentralNoGhosts(vec_x_wghosts.data(),param->potential,0,&(sim[0].solver),&(sim[0].mpi));
+  MPIExchangeBoundaries1D(mpi, vec_x_wghosts.data(), solver->dim_local[0], solver->ghosts, 0, solver->ndims);
+  if (!m_rank) {
+    std::cout << "checking - nabla phi (ghosts)\n";
+    for (int i = 0; i < vec_x_wghosts.size(); i++) {
+          std::cout << "Index " << i << ": " << -vec_x_wghosts[i]*(solver->dxinv[0])*(solver->dxinv[0])<< std::endl;
+    }
+    std::cout << std::endl;
+  }
+  exit (0);
 
   m_tic++;
   return;
