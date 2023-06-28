7ff3f931b7a465ab12ed09fc1f2fce7438bb9b02
Ping-Hsuan <pht2@illinois.edu>
2023-06-28 13:18:20 -0700

    free variables for potential

--- src/PhysicalModels/Vlasov/VlasovCleanup.c
+++ src/PhysicalModels/Vlasov/VlasovCleanup.c
@@ -12,16 +12,23 @@ int VlasovCleanup(void *s /*!< Solver object of type #HyPar */)
   Vlasov *physics = (Vlasov*) s;
   
   free(physics->e_field);
+  free(physics->potential);
 
 #ifdef fftw
   if(physics->self_consistent_electric_field) {
     free(physics->sum_buffer);
   
-    fftw_destroy_plan(physics->plan_forward);
-    fftw_destroy_plan(physics->plan_backward);
+    fftw_destroy_plan(physics->plan_forward_e);
+    fftw_destroy_plan(physics->plan_backward_e);
   
-    fftw_free(physics->phys_buffer);
-    fftw_free(physics->fourier_buffer);
+    fftw_free(physics->phys_buffer_e);
+    fftw_free(physics->fourier_buffer_e);
+
+    fftw_destroy_plan(physics->plan_forward_phi);
+    fftw_destroy_plan(physics->plan_backward_phi);
+
+    fftw_free(physics->phys_buffer_phi);
+    fftw_free(physics->fourier_buffer_phi);
   }
 #endif
 
