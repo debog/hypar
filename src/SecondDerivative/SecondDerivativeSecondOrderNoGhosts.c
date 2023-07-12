6784c1b8efade4c965cef5a300c613ff2c19eba9
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2023-07-12 12:11:32 -0700

    making 2nd order 2nd derivative function MPI-consistent

--- src/SecondDerivative/SecondDerivativeSecondOrderNoGhosts.c
+++ src/SecondDerivative/SecondDerivativeSecondOrderNoGhosts.c
@@ -8,6 +8,7 @@
 #include <basic.h>
 #include <arrayfunctions.h>
 #include <secondderivative.h>
+#include <mpivars.h>
 
 /*! Computes the second-order finite-difference approximation to the second derivative (\b Note: not divided by the grid spacing):
     \f{equation}{
@@ -20,23 +21,26 @@
     + Though the array D2f must include the same number of ghost points as the array f (including 0), 
       the second derivative is \b not computed at these locations. Thus, array elements corresponding 
       to the ghost points contain undefined values.
-    + \a D2f and \a f are 1D arrays containing the function and its computed derivatives on a multi-
-      dimensional grid. The derivative along the specified dimension \b dir is computed by looping
+    + \a D2f and \a f are serialized 1D arrays containing the function and its computed derivatives on 
+      a multi-dimensional grid. The derivative along the specified dimension \b dir is computed by looping
       through all grid lines along \b dir.
-    + Biased stencils are used at the boundary and ghost point values of f are not used. So this function 
-      can be used when the ghost values are not filled.
+    + Biased stencils are used at the physical boundaries and ghost point values of f are not used. So this 
+      function can be used when the ghost values at physical boundaries are not filled. The ghost values at
+      internal (MPI) boundaries are still needed.
 */
-int SecondDerivativeSecondOrderCentralNoGhosts( double  *D2f,   /*!< Array to hold the computed second derivative (with ghost points)
+int SecondDerivativeSecondOrderCentralNoGhosts( double*  D2f,   /*!< Array to hold the computed second derivative (with ghost points)
                                                                      (same size and layout as f) */
-                                                double  *f,     /*!< Array containing the grid point function values whose second
+                                                double*  f,     /*!< Array containing the grid point function values whose second
                                                                      derivative is to be computed (with ghost points) */
                                                 int     dir,    /*!< The spatial dimension along which the derivative is computed */
                                                 int     ndims,  /*!< Number of spatial/coordinate dimensions */
-                                                int     *dim,   /*!< Local dimensions */
+                                                int*    dim,    /*!< Local dimensions */
                                                 int     ghosts, /*!< Number of ghost points */
-                                                int     nvars   /*!< Number of vector components at each grid points */ )
+                                                int     nvars,  /*!< Number of vector components at each grid points */
+                                                void*   m       /*!< MPI object of type #MPIVariables */)
 {
-  int           i, v;
+  MPIVariables* mpi = (MPIVariables*) m;
+  int i, v;
 
   if ((!D2f) || (!f)) {
     fprintf(stderr, "Error in SecondDerivativeSecondOrder(): input arrays not allocated.\n");
@@ -51,8 +55,21 @@ int SecondDerivativeSecondOrderCentralNoGhosts( double  *D2f,   /*!< Array to ho
   int done = 0; _ArraySetValue_(index_outer,ndims,0);
   while (!done) {
     _ArrayCopy1D_(index_outer,indexC,ndims);
-    /* left boundary */
-    {
+    for (i = 0; i < dim[dir]; i++) {
+      int qL, qC, qR;
+      indexC[dir] = i-1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL);
+      indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC);
+      indexC[dir] = i+1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR);
+      for (v=0; v<nvars; v++)  D2f[qC*nvars+v] = f[qL*nvars+v]-2*f[qC*nvars+v]+f[qR*nvars+v];
+    }
+    _ArrayIncrementIndex_(ndims,bounds_outer,index_outer,done);
+  }
+
+  if (mpi->ip[dir] == 0) {
+    /* left physical boundary: overwrite the leftmost value with biased finite-difference */
+    int done = 0; _ArraySetValue_(index_outer,ndims,0);
+    while (!done) {
+      _ArrayCopy1D_(index_outer,indexC,ndims);
       i = 0;
       int qC, qR, qR2, qR3;
       indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC);
@@ -60,17 +77,15 @@ int SecondDerivativeSecondOrderCentralNoGhosts( double  *D2f,   /*!< Array to ho
       indexC[dir] = i+2; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR2);
       indexC[dir] = i+3; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR3);
       for (v=0; v<nvars; v++)  D2f[qC*nvars+v] = 2*f[qC*nvars+v]-5*f[qR*nvars+v]+4*f[qR2*nvars+v]-f[qR3*nvars+v];
+      _ArrayIncrementIndex_(ndims,bounds_outer,index_outer,done);
     }
-    /* interior */
-    for (i = 1; i < dim[dir]-1; i++) {
-      int qL, qC, qR;
-      indexC[dir] = i-1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL);
-      indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC);
-      indexC[dir] = i+1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR);
-      for (v=0; v<nvars; v++)  D2f[qC*nvars+v] = f[qL*nvars+v]-2*f[qC*nvars+v]+f[qR*nvars+v];
-    }
-    /* right boundary */
-    {
+  }
+
+  if (mpi->ip[dir] == mpi->iproc[dir]-1) {
+    /* right physical boundary: overwrite the rightmost value with biased finite-difference */
+    int done = 0; _ArraySetValue_(index_outer,ndims,0);
+    while (!done) {
+      _ArrayCopy1D_(index_outer,indexC,ndims);
       i = dim[dir]-1;
       int qL3, qL2, qL, qC;
       indexC[dir] = i-3; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL3);
@@ -78,9 +93,10 @@ int SecondDerivativeSecondOrderCentralNoGhosts( double  *D2f,   /*!< Array to ho
       indexC[dir] = i-1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL);
       indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC);
       for (v=0; v<nvars; v++)  D2f[qC*nvars+v] = 2*f[qC*nvars+v]-5*f[qL*nvars+v]+4*f[qL2*nvars+v]-f[qL3*nvars+v];
+      _ArrayIncrementIndex_(ndims,bounds_outer,index_outer,done);
     }
-    _ArrayIncrementIndex_(ndims,bounds_outer,index_outer,done);
   }
+
   
   return 0;
 }
