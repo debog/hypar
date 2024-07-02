b2e0ca68a211d3552bfd9f6779bb84b97c53b288
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2024-07-02 12:09:52 -0700

    updated the two-stream instability example inputs and postprocessing file

--- Examples/2D/Vlasov1D1V/TwoStreamInstability_logform/aux/post_process_log.c
+++ Examples/2D/Vlasov1D1V/TwoStreamInstability_logform/aux/post_process_log.c
@@ -1,7 +1,10 @@
-/* This code extracts the maximum E-field for the Landau
- * damping case from the electric field files efield_<nnnnn>.dat,
- * which are text files with three columns (grid index, x-coordinate,
- * and the e-field value). */
+/*
+  Two-Stream Instability (Log form):
+  The code takes a binary solution file (that contains the
+  log of the distribution function as its input
+  and writes out the distribution function to a Tecplot or
+  text file.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,78 +41,220 @@ void IncrementFilename(char *f)
   }
 }
 
+void WriteTecplot2D(int nvars,int imax, int jmax,double *x,double *u,char *f)
+{
+  printf("\tWriting tecplot solution file %s.\n",f);
+  FILE *out;
+  out = fopen(f,"w");
+  if (!out) {
+    fprintf(stderr,"Error: could not open %s for writing.\n",f);
+    return;
+  }
 
-int main() {
-    int NI, NJ, ndims;
-    double dt, samay;
-    int file_op_iter, restart_iter = 0;
-    char filename[50];
+  double *X = x;
+  double *Y = x+imax;
 
-    FILE *in = fopen("solver.inp", "r");
-    if (!in) {
-        fprintf(stderr, "Error: File \"solver.inp\" not found.\n");
-        return 1;
-    } else {
-        char word[100];
-        fscanf(in, "%s", word);
-        if (!strcmp(word, "begin")) {
-            while (strcmp(word, "end")) {
-                fscanf(in, "%s", word);
-                if (!strcmp(word, "ndims")) fscanf(in, "%d", &ndims);
-                else if (!strcmp(word, "size")) {
-                    fscanf(in, "%d", &NI);
-                    fscanf(in, "%d", &NJ);
-                } else if (!strcmp(word, "dt")) fscanf(in, "%lf", &dt);
-                else if (!strcmp(word, "file_op_iter")) fscanf(in, "%d", &file_op_iter);
-                else if (!strcmp(word, "restart_iter")) fscanf(in, "%d", &restart_iter);
-            }
-        }
-        fclose(in);
+  /* writing tecplot data file headers */
+  fprintf(out,"VARIABLES=\"I\",\"J\",\"X\",\"Y\",\"F\",\n");
+  fprintf(out,"ZONE I=%d,J=%d,F=POINT\n",imax,jmax);
+
+  /* writing the data */
+  int i,j;
+  for (j=0; j<jmax; j++) {
+    for (i=0; i<imax; i++) {
+      int v, p = i + imax*j;
+      fprintf(out,"%4d %4d ",i,j);
+      fprintf(out,"%1.16E %1.16E ",X[i],Y[j]);
+      for (v=0; v<nvars; v++) fprintf(out,"%1.16E ",u[nvars*p+v]);
+      fprintf(out,"\n");
     }
+  }
+  fclose(out);
+  return;
+}
 
-    samay = (double)restart_iter * dt;
-    strcpy(filename, "op_00000.dat");
-    for (int t = 0; t < restart_iter; t++) {
-        if ((t + 1) % file_op_iter == 0) IncrementFilename(filename);
+void WriteText2D(int nvars,int imax, int jmax,double *x,double *u,char *f)
+{
+  printf("\tWriting text solution file %s.\n",f);
+  FILE *out;
+  out = fopen(f,"w");
+  if (!out) {
+    fprintf(stderr,"Error: could not open %s for writing.\n",f);
+    return;
+  }
+
+  double *X = x;
+  double *Y = x+imax;
+
+  /* writing the data */
+  int i,j;
+  for (j=0; j<jmax; j++) {
+    for (i=0; i<imax; i++) {
+      int v, p = i + imax*j;
+      fprintf(out,"%4d %4d ",i,j);
+      fprintf(out,"%1.16E %1.16E ",X[i],Y[j]);
+      for (v=0; v<nvars; v++) fprintf(out,"%1.16E ",u[nvars*p+v]);
+      fprintf(out,"\n");
     }
+  }
+  fclose(out);
+  return;
+}
 
-    while (1) {
-        char out_filename[50];
-        strcpy(out_filename, filename);
-        out_filename[0] = 'p';  // Change the prefix from 'o' to 'p'
+int PostProcess(char *fname, char *oname, int flag)
+{
+  FILE *in; in = fopen(fname,"rb");
 
-        FILE *in_file = fopen(filename, "r");
-        if (!in_file) {
-            fprintf(stderr, "Error: File \"%s\" not found.\n", filename);
-            break;
-        }
+  if (!in) return(-1);
 
-        FILE *out_file = fopen(out_filename, "w");
-        if (!out_file) {
-            fprintf(stderr, "Error: Could not open \"%s\" for writing.\n", out_filename);
-            fclose(in_file);
-            break;
-        }
+  printf("Reading file %s.\n",fname);
+  int ndims, nvars;
+  double *U,*x;
 
-        char buffer[256];
-        // Skip two header lines
-        fgets(buffer, sizeof(buffer), in_file);
-        fputs(buffer, out_file); // Copy the first header line
-        fgets(buffer, sizeof(buffer), in_file);
-        fputs(buffer, out_file); // Copy the second header line
+  /* read the file headers */
+  fread(&ndims,sizeof(int),1,in);
+  fread(&nvars,sizeof(int),1,in);
 
-        int i, j;
-        double x, y, value;
-        while (fscanf(in_file, "%d %d %lf %lf %lf", &i, &j, &x, &y, &value) == 5) {
-            double exp_value = exp(value);
-            fprintf(out_file, "%d %d %1.16e %1.16e %1.16e\n", i, j, x, y, exp_value);
-        }
+  /* some checks */
+  if (ndims != 2) {
+    printf("Error: ndims in %s not equal to 2!\n",fname);
+    return(1);
+  }
+  if (nvars != 1) {
+    printf("Error: nvars in %s not equal to 4!\n",fname);
+    return(1);
+  }
 
-        fclose(in_file);
-        fclose(out_file);
+  /* read dimensions */
+  int dims[ndims];
+  fread(dims,sizeof(int),ndims,in);
+  printf("Dimensions: %d x %d\n",dims[0],dims[1]);
+  printf("Nvars     : %d\n",nvars);
 
-        IncrementFilename(filename);  // Increment to the next input and output file
+  /* allocate grid and solution arrays */
+  x = (double*) calloc (dims[0]+dims[1]       ,sizeof(double));
+  U = (double*) calloc (dims[0]*dims[1]*nvars ,sizeof(double));
+
+  /* read grid and solution */
+  fread(x,sizeof(double),dims[0]+dims[1]      ,in);
+  fread(U,sizeof(double),dims[0]*dims[1]*nvars,in);
+  /* done reading */
+  fclose(in);
+
+  int imax = dims[0];
+  int jmax = dims[1];
+
+  /* allocate distribution function array */
+  double *Q = (double*) calloc (imax*jmax,sizeof(double));
+
+  /* calculate primitive variables */
+  int i, j;
+  double *X           = x;
+  double *Y           = x+imax;
+
+  for (i=0; i<imax; i++) {
+    for (j=0; j<jmax; j++) {
+      int p = i + imax*j;
+      Q[p] = exp(U[p]);
     }
+  }
 
-    return 0;
+  /* write Tecplot/Text file */
+  if (flag) WriteTecplot2D(1,imax,jmax,x,Q,oname);
+  else      WriteText2D   (1,imax,jmax,x,Q,oname);
+
+  /* clean up */
+  free(U);
+  free(Q);
+  free(x);
+}
+
+int main()
+{
+  FILE *out1, *out2, *in, *inputs;
+  char filename[50], op_file_format[50], tecfile[50], overwrite[50];
+
+  int flag;
+  printf("Write tecplot file (1) or plain text file (0): ");
+  scanf("%d",&flag);
+
+  if ((flag != 1) && (flag != 0)) {
+    printf("Error: Invalid input. Should be 1 or 0.\n");
+    return(0);
+  }
+
+  printf("Reading solver.inp.\n");
+  inputs = fopen("solver.inp","r");
+  if (!inputs) {
+    fprintf(stderr,"Error: File \"solver.inp\" not found.\n");
+    return(1);
+  } else {
+    char word[100];
+    fscanf(inputs,"%s",word);
+    if (!strcmp(word, "begin")){
+      while (strcmp(word, "end")){
+        fscanf(inputs,"%s",word);
+         if      (!strcmp(word, "op_file_format"   ))  fscanf(inputs,"%s" ,op_file_format);
+         else if (!strcmp(word, "op_overwrite"     ))  fscanf(inputs,"%s" ,overwrite      );
+      }
+    }
+    fclose(inputs);
+  }
+  if (strcmp(op_file_format,"binary") && strcmp(op_file_format,"bin")) {
+    printf("Error: solution output needs to be in binary files.\n");
+    return(0);
+  }
+
+  int use_log_form = 0; /* default in HyPar is 0 */
+  printf("Reading physics.inp.\n");
+  inputs = fopen("physics.inp","r");
+  if (!inputs) {
+    fprintf(stderr,"Error: File \"physics.inp\" not found.\n");
+    return(1);
+  } else {
+    char word[100];
+    fscanf(inputs,"%s",word);
+    if (!strcmp(word, "begin")){
+      while (strcmp(word, "end")){
+        fscanf(inputs,"%s",word);
+        if      (!strcmp(word, "use_log_form"))    fscanf(inputs,"%d",&use_log_form);
+      }
+    }
+    fclose(inputs);
+  }
+  if (!use_log_form) {
+    printf("Error: \"use_log_form\" is 0 or unspecified in physics.inp.\n");
+    return(0);
+  }
+
+  if (!strcmp(overwrite,"no")) {
+    strcpy(filename,"op_00000.bin");
+    while(1) {
+      /* set filename */
+      strcpy(tecfile,filename);
+      tecfile[9]  = 'd';
+      tecfile[10] = 'a';
+      tecfile[11] = 't';
+      int err = PostProcess(filename, tecfile, flag);
+      if (err == -1) {
+        printf("No more files found. Exiting.\n");
+        break;
+      }
+      IncrementFilename(filename);
+    }
+  } else if (!strcmp(overwrite,"yes")) {
+    strcpy(filename,"op.bin");
+    /* set filename */
+    strcpy(tecfile,filename);
+    tecfile[3] = 'd';
+    tecfile[4] = 'a';
+    tecfile[5] = 't';
+    int err = PostProcess(filename, tecfile, flag);
+    if (err == -1) {
+      printf("Error: op.bin not found.\n");
+      return(0);
+    }
+  }
+
+  return(0);
 }
