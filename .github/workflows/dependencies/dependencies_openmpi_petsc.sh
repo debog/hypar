8c4a725e5a4cb1bc5983a445f5d4ed1716fe9900
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-11-06 17:34:19 -0800

    making PETSc cloning more robust

--- .github/workflows/dependencies/dependencies_openmpi_petsc.sh
+++ .github/workflows/dependencies/dependencies_openmpi_petsc.sh
@@ -9,7 +9,11 @@ sudo apt-get install -y --no-install-recommends\
     g++ gfortran openmpi-bin libopenmpi-dev \
     libblas-dev liblapack-dev
 
-git clone -b release https://gitlab.com/petsc/petsc.git ../petsc
+for i in {1..5}; do
+  git clone --depth=1 -b release https://gitlab.com/petsc/petsc.git ../petsc && break
+  echo "Clone failed, retrying ($i/5)..."
+  sleep 10
+done
 pushd ../petsc
 export PETSC_DIR=$PWD
 export PETSC_ARCH=arch-linux-gcc
