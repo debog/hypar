0efeb49e875d170cff2905fef4faf742b32dd27a
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2025-12-02 11:02:16 -0800

    removed temporary scripts

--- fix_stride.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/ghosh/Codes/hypar
-
-# Replace all usages of _NavierStokes3D_stride_ with s__NavierStokes3D_stride_
-find . -type f \( -name "*.c" -o -name "*.cu" -o -name "*.cpp" -o -name "*.h" \) \
-  -exec sed -i 's/_NavierStokes3D_stride_/s__NavierStokes3D_stride_/g' {} \;
-
-echo "Fixed _NavierStokes3D_stride_ variable usages"
